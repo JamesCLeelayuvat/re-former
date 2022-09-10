e6fafa7b05d71d5625ea9af842aeec654dd5d8cd
JamesCLeelayuvat <Jmsleelayuvat@gmail.com>
2022-09-10 12:33:44 +0700

    commit all

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    puts @user.errors.full_messages
+      redirect_to @user
+    else
+        puts @user.errors.full_messages
+
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
